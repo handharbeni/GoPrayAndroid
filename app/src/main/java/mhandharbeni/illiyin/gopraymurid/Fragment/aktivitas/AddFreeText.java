93cd9067260506b077a38dbfe4fed7d6e62d8ea3
handharbeni <mhandharbeni@gmail.com>
2017-05-14 23:37:51 +0700

    Update Timeline

--- app/src/main/java/mhandharbeni/illiyin/gopraymurid/Fragment/aktivitas/AddFreeText.java
+++ app/src/main/java/mhandharbeni/illiyin/gopraymurid/Fragment/aktivitas/AddFreeText.java
@@ -1,8 +1,22 @@
 package mhandharbeni.illiyin.gopraymurid.Fragment.aktivitas;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mhandharbeni.illiyin.gopraymurid.R;
+
 /**
  * Created by root on 12/05/17.
  */
 
-public class AddFreeText {
+public class AddFreeText extends Fragment {
+    View v;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.fragment_family, container, false);
+        return v;
+    }
 }
